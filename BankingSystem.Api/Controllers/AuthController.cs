0277b28c4fc37aae9d487493ed09017a86789dfd
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-02-13 17:20:51 +0400

    add AuthController

---
+++ BankingSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingSystem.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+	}
+}

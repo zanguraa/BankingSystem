c96a92cc77593eb4cc384a362f6a4049e43e2def
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-02-16 16:12:54 +0400

    Create User.cs Implementation

--- BankingSystem.Core/Models/User.cs
+++ BankingSystem.Core/Models/User.cs
@@ -2,6 +2,22 @@
 {
     public class User
     {
+	
 
+		public string Id { get; set; }
+        public string Name { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public User(string name , string lastname,  string email , string password, DateTime birthdate)
+		{
+			Name = name;
+            LastName = lastname;
+            Email=email;
+            Password = password;
+            birthdate = birthdate;
+                 
+		}
     }
 }

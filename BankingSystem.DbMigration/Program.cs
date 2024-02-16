6aadca41b4a82e1430c732c63f5413c5992368af
Zangura <zanguraa@gmail.com>
2024-02-16 17:29:13 +0400

    add connection strings config for DbUp library

--- BankingSystem.DbMigration/Program.cs
+++ BankingSystem.DbMigration/Program.cs
@@ -1,10 +1,24 @@
-﻿public class Program
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using DbUp;
+using DbUp.SqlServer;
+
+
+public class Program
 {
     static int Main(string[] args)
     {
-        var connectionString =
-            args.FirstOrDefault()
-            ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var user = "Zangura";
+
+        var connectionString = config.GetConnectionString(user);
 
         var upgrader =
             DeployChanges.To
@@ -21,7 +35,7 @@
             Console.WriteLine(result.Error);
             Console.ResetColor();
 #if DEBUG
-        Console.ReadLine();
+            Console.ReadLine();
 #endif
             return -1;
         }

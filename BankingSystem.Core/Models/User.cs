922fe8a4d3e7f37ba0590d80336aae50891eaa9e
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-02-16 16:23:33 +0400

    Add PersonalNumber in User.cs

--- BankingSystem.Core/Models/User.cs
+++ BankingSystem.Core/Models/User.cs
@@ -6,14 +6,16 @@
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+		public string PersonalNumber { get; set; }
+		public string Password { get; set; }
         public DateTime BirthDate { get; set; }
-        public User(string id , string name , string lastname,  string email , string password, DateTime birthDate)
+        public User(string id , string name , string lastname,  string email , string personalnumber , string password, DateTime birthDate)
 		{
             Id = id;
 			Name = name;
             LastName = lastname;
             Email=email;
+            PersonalNumber = personalnumber;
             Password = password;
             BirthDate = birthDate;
 		}

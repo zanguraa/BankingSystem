b4253ca86556546727a4ba23feadedb41c78bd5e
Zangura <zanguraa@gmail.com>
2024-02-18 13:31:22 +0400

    add CreateOperatorTable.sql file in DbMigration project

---
+++ BankingSystem.DbMigration/scripts/Script002-CreateOperatorTable.sql
@@ -0,0 +1,15 @@
+USE BankingSystem_db;
+
+IF NOT EXISTS (SELECT * FROM [BankingSystem_db] WHERE TABLE_NAME = 'Operator')
+BEGIN
+    CREATE TABLE Operator
+    (
+        OperatorId INT IDENTITY(1,1) PRIMARY KEY,
+        Username NVARCHAR(50) NOT NULL,
+        Password NVARCHAR(100) NOT NULL
+    );
+END;
+ELSE
+BEGIN
+    PRINT 'The table Operator already exists.';
+END;

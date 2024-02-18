d25aaf0052935deb60e9ccb98d5aa6efa4f1e8cb
Zangura <zanguraa@gmail.com>
2024-02-18 22:52:27 +0400

    add PasswordHasher method in shared folder

--- BankingSystem.Core/Repositories/OperatorRepository.cs
+++ BankingSystem.Core/Repositories/OperatorRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BankingSystem.Core.Interfaces;
 using BankingSystem.Core.Models.Requests;
+using BankingSystem.Core.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,7 @@ namespace BankingSystem.Core.Repositories
 
         public bool AddOperator(CreateOperatorRequest request)
         {
+            request.Password = PasswordHasher.HashHmacSHA256(request.Password);
             string sql = "INSERT INTO Operator ( Username, Password) VALUES (@Username, @Password)";
             return _dataManager.Execute(sql, request) > 0;
         }

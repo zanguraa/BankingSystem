e940cc7eb4dc3ddd84ddd8c2fdf270e4c79cddf3
Zangura <zanguraa@gmail.com>
2024-02-22 23:59:56 +0400

    remove fields from UserEntity: Id, Password and Email is in IdentityUser already

--- BankingSystem.Core/Data/UserEntity.cs
+++ BankingSystem.Core/Data/UserEntity.cs
@@ -7,18 +7,14 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Data
 {
-	public class UserEntity : IdentityUser<int>
-	{
-		public DateTime BirthdayDate { get; set; }
-		public string PersonalId { get; set; }
-		public string PhoneNumber { get; set; }
-		public int Id { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string PersonalNumber { get; set; }
-		public string Email { get; set; }
-		public string Password { get; set; }
-		public virtual ICollection<AccountEntity> Accounts { get; set; }
-
-	}
+    public class UserEntity : IdentityUser<int>
+    {
+        public DateTime BirthdayDate { get; set; }
+        public string PersonalId { get; set; } 
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonalNumber { get; set; }
+        public virtual ICollection<AccountEntity> Accounts { get; set; }
+    }
 }
--- BankingSystem.Core/Services/UserService.cs
+++ BankingSystem.Core/Services/UserService.cs
@@ -1,58 +1,51 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using BankingSystem.Core.Data;
 using BankingSystem.Core.Models.Requests;
+using BankingSystem.Core.Data;
 
 namespace BankingSystem.Core.Services
 {
-	public class UserService
-	{//  in-memory საცავი მომხმარებლებისთვის 
-		private readonly List<UserEntity> _users = new List<UserEntity>();
+    public class UserService
+    {
+        private readonly UserManager<UserEntity> _userManager;
 
-		public async Task<UserEntity> RegisterUser(AccountRegisterRequest registerRequest)
-		{
-			// ამოწმებს ემაილის უნიკალურობას
-			if (_users.Any(u => u.Email == registerRequest.Email))
-			{
-				throw new ApplicationException("Email is already registered");
-			}
+        public UserService(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
 
-			// ჰეშავს პაროლს
-			string hashedPassword = HashPassword(registerRequest.Password);
+        public async Task<UserEntity> RegisterUser(AccountRegisterRequest registerRequest)
+        {
+            var existingUser = await _userManager.FindByEmailAsync(registerRequest.Email);
+            if (existingUser != null)
+            {
+                throw new ApplicationException("Email is already registered");
+            }
 
+            var newUser = new UserEntity
+            {
+                FirstName = registerRequest.FirstName,
+                LastName = registerRequest.LastName,
+                PersonalNumber = registerRequest.PersonalNumber, // Make sure to set PersonalNumber
+                BirthdayDate = registerRequest.BirthdayDate,
+                Email = registerRequest.Email,
+            };
 
-			var newUser = new UserEntity
-			{
-				FirstName = registerRequest.FirstName,
-				LastName = registerRequest.LastName,
-				PersonalNumber = registerRequest.PersonalNumber,
-				BirthdayDate = registerRequest.BirthdayDate,
-				Email = registerRequest.Email,
-				Password = hashedPassword
-			};
+            var result = await _userManager.CreateAsync(newUser, registerRequest.Password);
+            if (!result.Succeeded)
+            {
+                throw new ApplicationException("Failed to create user");
+            }
 
-			// ახალ იუზერს ანიჭებს უნიკალურ userId ს
-			newUser.Id = _users.Count + 1;
+            return newUser;
+        }
 
-			// იუზერს ამატებს საცავში
-			_users.Add(newUser);
-
-			return newUser;
-		}
-
-		public async Task<UserEntity> GetUserByEmail(string email)
-		{
-			// იუზერს პოულობს ემაილით
-			return _users.FirstOrDefault(u => u.Email == email);
-		}
-
-		private string HashPassword(string password)
-		{
-			return password;
-		}
-	}
+        public async Task<UserEntity> GetUserByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+    }
 }
-

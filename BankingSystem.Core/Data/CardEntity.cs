b152fa28a86bc7d52fa5c3259f872b8693a7acca
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-02-22 15:54:32 +0400

    Add Declare Properties As Nullable

--- BankingSystem.Core/Data/CardEntity.cs
+++ BankingSystem.Core/Data/CardEntity.cs
@@ -8,6 +8,12 @@ namespace BankingSystem.Core.Data
 {
 	public class CardEntity
 	{
+		public int Id { get; set; }
+		public string CardNumber { get; set; }
+		public string CardHolderName { get; set; }
+		public DateTime CardExpirationDate { get; set; }
+		public string CvvCode { get; set; }
+		public string PinCode { get; set; }
 
 	}
 }

23132b00144fd69161d544491d129770c51bb73a
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-02-22 15:42:10 +0400

    Add AccountEntity Implementation / properties

--- BankingSystem.Core/Data/AccountEntity.cs
+++ BankingSystem.Core/Data/AccountEntity.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Data
 {
-	internal class AccountEntity
+	public class AccountEntity
 	{
+		public int Id { get; set; }
+		public string? Iban { get; set; }
+		public decimal Amount { get; set; }
+		public string? Currency { get; set; }
+		public int UserId { get; set; }
+		public virtual UserEntity? User { get; set; }
+
 	}
 }

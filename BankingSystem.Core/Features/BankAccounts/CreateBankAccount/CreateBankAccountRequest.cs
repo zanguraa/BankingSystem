f0e011b50d023650fc790b41fcbdaa7e6008af1c
Zangura <zanguraa@gmail.com>
2024-02-24 23:53:24 +0400

    add currency types for GEL, USD and EUR

--- BankingSystem.Core/Features/BankAccounts/CreateBankAccount/CreateBankAccountRequest.cs
+++ BankingSystem.Core/Features/BankAccounts/CreateBankAccount/CreateBankAccountRequest.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Features.BankAccounts.CreateBankAccount
 {
+
+    public enum CurrencyType
+    {
+        USD = 840,  // United States Dollar
+        EUR = 978,  // Euro
+        GEL = 981   // Georgian Lari
+    }
+
     public class CreateBankAccountRequest
     {
         public Guid UserId { get; set; }
         public string Iban { get; set; }
         public decimal InitialAmount { get; set; }
-        public string Currency { get; set; }
+        public CurrencyType Currency { get; set; }
     }
 }

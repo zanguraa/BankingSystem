0c03397f3c44ac0e1cd82dc86b4f5f7e5b8cc3f2
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-01 15:27:47 +0400

    Add TransactionController.cs

--- BankingSystem.Api/Controllers/TransactionController.cs
+++ BankingSystem.Api/Controllers/TransactionController.cs
@@ -1,37 +1,37 @@
-﻿//using BankingSystem.Core.Features.Transactions.CreateTransaction;
-//using BankingSystem.Core.Features.Transactions.TransactionService;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
+﻿using BankingSystem.Core.Features.Transactions.CreateTransaction;
+using BankingSystem.Core.Features.Transactions.TransactionService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace BankingSystem.Api.Controllers
-//{
-//	[Route("api/[controller]")]
-//	[ApiController]
-//	public class TransactionController : ControllerBase
-//	{
-//		private readonly ITransactionService _transactionService;
+namespace BankingSystem.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TransactionController : ControllerBase
+	{
+		private readonly ITransactionService _transactionService;
 
-//		public TransactionController(ITransactionService transactionService)
-//		{
-//			_transactionService = transactionService;
-//		}
+		public TransactionController(ITransactionService transactionService)
+		{
+			_transactionService = transactionService;
+		}
 
-//		[HttpPost("create")]
-//		public async Task<IActionResult> CreateTransaction(CreateTransactionRequest request)
-//		{
-//			try
-//			{
-//				var transactionResponse = await _transactionService.CreateTransactionAsync(request);
-//				return Ok(transactionResponse);
-//			}
-//			catch (ArgumentException ex)
-//			{
-//				return BadRequest(ex.Message);
-//			}
-//			catch (Exception ex)
-//			{
-//				return StatusCode(500, "Internal server error");
-//			}
-//		}
-//	}
-//}
+		[HttpPost("create")]
+		public async Task<IActionResult> CreateTransaction(CreateTransactionRequest request)
+		{
+			try
+			{
+				var transactionResponse = await _transactionService.CreateTransactionAsync(request);
+				return Ok(transactionResponse);
+			}
+			catch (ArgumentException ex)
+			{
+				return BadRequest(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, "Internal server error");
+			}
+		}
+	}
+}

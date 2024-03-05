47c79f27c698a7ca7ad72f219443b1745d80abfe
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-06 01:45:21 +0400

    Add ChangePinRepository.cs  , ChangePinService.cs , IChangePinRepository.cs , IChangePinService.cs   full Implementation

--- BankingSystem.Core/Features/Atm/ChangePin/IChangePinRepository.cs
+++ BankingSystem.Core/Features/Atm/ChangePin/IChangePinRepository.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BankingSystem.Core.Features.Cards;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using BankingSystem.Core.Features.Cards;
 
-namespace BankingSystem.Core.Features.Atm.ChangePin
-{
-	public interface IChangePinRepository
-	{
-		Task<Card> GetCardByNumberAsync(string cardNumber);
-		Task<bool> UpdatePinAsync(string cardNumber, string currentPin, string newPin);
-	}
-}
+//namespace BankingSystem.Core.Features.Atm.ChangePin
+//{
+//	public interface IChangePinRepository
+//	{
+//		Task<Card> GetCardByNumberAsync(string cardNumber);
+//		Task<bool> UpdatePinAsync(string cardNumber, string currentPin, string newPin);
+//	}
+//}
--- BankingSystem.DbMigration/Scripts/Script009 -  CreateChangePinAttemptsTable.sql
+++ BankingSystem.DbMigration/Scripts/Script009 -  CreateChangePinAttemptsTable.sql
@@ -1,11 +1,11 @@
-USE BankingSystem_db
-GO
+--USE BankingSystem_db
+--GO
 
-    CREATE TABLE ChangePinAttempts
-    (
-        ChangeId INT IDENTITY(1,1) PRIMARY KEY,
-        CardNumber NVARCHAR(16) NOT NULL,
-        ChangeTime DATETIME NOT NULL DEFAULT GETDATE(),
-        WasSuccessful BIT NOT NULL,
-        CONSTRAINT FK_ChangePinAttempts_Cards FOREIGN KEY (CardNumber) REFERENCES Cards(CardNumber)
-    );
+--    CREATE TABLE ChangePinAttempts
+--    (
+--        ChangeId INT IDENTITY(1,1) PRIMARY KEY,
+--        CardNumber NVARCHAR(16) NOT NULL,
+--        ChangeTime DATETIME NOT NULL DEFAULT GETDATE(),
+--        WasSuccessful BIT NOT NULL,
+--        CONSTRAINT FK_ChangePinAttempts_Cards FOREIGN KEY (CardNumber) REFERENCES Cards(CardNumber)
+--    );

afc34fbfd9e4dc08fb9e985660639bb3bbdca092
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-05 17:22:18 +0400

    Add Full Implementation of ICardAuthorizationRepository.cs

--- BankingSystem.Core/Features/Atm/CardAuthorizations/ICardAuthorizationRepository.cs
+++ BankingSystem.Core/Features/Atm/CardAuthorizations/ICardAuthorizationRepository.cs
@@ -2,5 +2,10 @@
 {
 	public interface ICardAuthorizationRepository
 	{
+		Task<BankAccount> GetBankAccountByCardNumberAsync(string cardNumber);
+		Task<bool> UpdatePinCodeAsync(string cardNumber, string newPinHash);
+		Task LogAuthorizationAttemptAsync(string cardNumber, bool isSuccess);
+		Task<bool> IsCardActivatedAsync(string cardNumber);
+
 	}
 }

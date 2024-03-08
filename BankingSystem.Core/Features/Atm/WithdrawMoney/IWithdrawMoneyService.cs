2e24ad6c1ab8ec14acaecc0de317fafc4a9a04c0
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-08 16:33:28 +0400

    Add WithdrawMoneyController.cs

--- BankingSystem.Core/Features/Atm/WithdrawMoney/IWithdrawMoneyService.cs
+++ BankingSystem.Core/Features/Atm/WithdrawMoney/IWithdrawMoneyService.cs
@@ -1,6 +1,9 @@
-﻿namespace BankingSystem.Core.Features.Atm.WithdrawMoney
+﻿using BankingSystem.Core.Features.Atm.WithdrawMoney.Dto_s;
+
+namespace BankingSystem.Core.Features.Atm.WithdrawMoney
 {
 	public interface IWithdrawMoneyService
 	{
+		Task<WithdrawResponseDto> WithdrawAsync(WithdrawRequestDto requestDto);
 	}
 }

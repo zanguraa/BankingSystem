06831b7baa4fad081c49e73c2f9475b6945ba96b
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-12 16:34:40 +0400

    Add Dto Files Logic

--- BankingSystem.Core/Features/Reports/Dto's/DailyTransactionCountDto.cs
+++ BankingSystem.Core/Features/Reports/Dto's/DailyTransactionCountDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Features.Reports.Dto_s
 {
-	internal class DailyTransactionCountDto
+	public class DailyTransactionCountDto
 	{
 	}
 }
--- BankingSystem.Core/Features/Reports/Dto's/UserStatisticsDto.cs
+++ BankingSystem.Core/Features/Reports/Dto's/UserStatisticsDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Features.Reports.Dto_s
 {
-	internal class UserStatisticsDto
+	public class UserStatisticsDto
 	{
 	}
 }

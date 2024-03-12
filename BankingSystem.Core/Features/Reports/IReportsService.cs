9d0475005c3f1aeac5eb630347785c3b687e7a3b
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-12 19:41:08 +0400

    Update UserStatisticsDto.cs ,WithdrawnAmountByCurrencyDto.cs and Interfaces

--- BankingSystem.Core/Features/Reports/IReportsService.cs
+++ BankingSystem.Core/Features/Reports/IReportsService.cs
@@ -1,10 +1,10 @@
-﻿//using BankingSystem.Core.Features.Reports.Dto_s;
+﻿using BankingSystem.Core.Features.Reports.Dto_s;
 
-//public interface IReportsService
-//{
-//	Task<TransactionStatisticsDto> GetTransactionStatisticsAsync(DateTime startDate, DateTime endDate);
-//	Task<IEnumerable<DailyTransactionCountDto>> GetDailyTransactionCountsAsync(DateTime startDate, DateTime endDate);
-//	Task<TotalWithdrawnAmountDto> GetTotalWithdrawnAmountAsync(DateTime startDate, DateTime endDate);
-//	Task<TransactionStatisticsDto> GetAverageRevenuePerTransactionAsync(DateTime startDate, DateTime endDate);
-//	Task<UserStatisticsDto> GetUserStatisticsAsync();
-//}
+public interface IReportsService
+{
+	Task<TransactionStatisticsDto> GetTransactionStatisticsAsync(DateTime startDate, DateTime endDate);
+	Task<IEnumerable<DailyTransactionCountDto>> GetDailyTransactionCountsAsync(DateTime startDate, DateTime endDate);
+	Task<TotalWithdrawnAmountDto> GetTotalWithdrawnAmountAsync(DateTime startDate, DateTime endDate);
+	Task<TransactionStatisticsDto> GetAverageRevenuePerTransactionAsync(DateTime startDate, DateTime endDate);
+	Task<UserStatisticsDto> GetUserStatisticsAsync();
+}

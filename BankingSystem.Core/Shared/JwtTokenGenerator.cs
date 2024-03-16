232c5262a92940f810d0f6819fcd800f7af4b5a5
zangura <zanguraa@gmail.com>
2024-03-16 15:57:12 +0400

    add AtmTokenGenerator method in JwtTokenGenerator class

--- BankingSystem.Core/Shared/JwtTokenGenerator.cs
+++ BankingSystem.Core/Shared/JwtTokenGenerator.cs
@@ -3,43 +3,57 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BankingSystem.Core.Shared
 {
-	public class JwtTokenGenerator
-	{
-		private readonly IConfiguration configuration;
+    public class JwtTokenGenerator
+    {
+        private readonly IConfiguration configuration;
 
-		public JwtTokenGenerator(IConfiguration configuration)
-		{
-			this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-		}
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
-		public string Generate(string userId, string userRole)
-		{
-			var claims = new List<Claim>
-		{
-			new Claim(JwtRegisteredClaimNames.Sub, userId),
-			new Claim(ClaimTypes.Role, userRole)
-		};
+        public string Generate(string userId, string userRole)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(ClaimTypes.Role, userRole)
+            };
 
-			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtTokenSecretKey"]!));
-			var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            return GenerateToken(claims);
+        }
 
-			var token = new JwtSecurityToken(
-				issuer: "myapp.com",
-				audience: "myapp.com",
-				claims: claims,
-				expires: DateTime.Now.AddMinutes(3600),
-				signingCredentials: credentials);
+        public string GenerateTokenForAtmOperations(string cardNumber, string pinCode)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("CardNumber", cardNumber),
+                new Claim("PinCode", pinCode)
+            };
 
-			var tokenGenerator = new JwtSecurityTokenHandler();
-			var jwt = tokenGenerator.WriteToken(token);
-			return jwt;
-		}
-	}
+            return GenerateToken(claims);
+        }
+
+        private string GenerateToken(List<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtTokenSecretKey"]!));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "myapp.com",
+                audience: "myapp.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(3600), // Adjust token lifetime as necessary
+                signingCredentials: credentials);
+
+            var tokenGenerator = new JwtSecurityTokenHandler();
+            var jwt = tokenGenerator.WriteToken(token);
+            return jwt;
+        }
+    }
 }

11f12f01e827dbfc3ee59c862c180e15cb03ac68
zangura <zanguraa@gmail.com>
2024-03-24 11:50:03 +0400

    remove ICardAuthorizationService and ICardAuthorizationRepository interfaces, implement inside files

--- BankingSystem.Core/Features/Atm/CardAuthorizations/CardAuthorizationService.cs
+++ BankingSystem.Core/Features/Atm/CardAuthorizations/CardAuthorizationService.cs
@@ -1,26 +1,30 @@
-﻿using BankingSystem.Core.Features.Atm.CardAuthorization;
-using BankingSystem.Core.Features.Atm.CardAuthorizations.Requests;
+﻿using BankingSystem.Core.Features.Atm.CardAuthorizations.Requests;
 using BankingSystem.Core.Features.Cards;
+
+public interface ICardAuthorizationService
+{
+    Task<bool> AuthorizeCardAsync(CardAuthorizationRequest request);
+}
 
 public class CardAuthorizationService : ICardAuthorizationService
 {
-	private readonly ICardAuthorizationRepository _cardAuthorizationRepository;
+    private readonly ICardAuthorizationRepository _cardAuthorizationRepository;
 
-	public CardAuthorizationService(
-		ICardAuthorizationRepository cardAuthorizationRepository)
-	{
-		_cardAuthorizationRepository = cardAuthorizationRepository;
-	}
+    public CardAuthorizationService(
+        ICardAuthorizationRepository cardAuthorizationRepository)
+    {
+        _cardAuthorizationRepository = cardAuthorizationRepository;
+    }
 
-	public async Task<bool> AuthorizeCardAsync(CardAuthorizationRequest request)
-	{
-		var card = await _cardAuthorizationRepository.GetCardFromRequestAsync(request);
-		return card != null && !IsCardExpired(card) && card.IsActive;
-	}
+    public async Task<bool> AuthorizeCardAsync(CardAuthorizationRequest request)
+    {
+        var card = await _cardAuthorizationRepository.GetCardFromRequestAsync(request);
+        return card != null && !IsCardExpired(card) && card.IsActive;
+    }
 
-	
-	private bool IsCardExpired(Card card )
-	{
-		return card.ExpirationDate < DateTime.UtcNow;
-	}
+
+    private bool IsCardExpired(Card card)
+    {
+        return card.ExpirationDate < DateTime.UtcNow;
+    }
 }

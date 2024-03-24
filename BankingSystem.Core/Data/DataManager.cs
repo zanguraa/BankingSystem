741a0a58b3e3ce44a396dc5d38a153890e2981b8
zangura <zanguraa@gmail.com>
2024-03-24 11:43:42 +0400

    remove IDatamanager interface, implement IDatamanager inside Datamanager class

--- BankingSystem.Core/Data/DataManager.cs
+++ BankingSystem.Core/Data/DataManager.cs
@@ -5,25 +5,33 @@ using System.Data;
 
 namespace BankingSystem.Core.Data
 {
-    public class DataManager : IDataManager
-	{
-		private readonly string _connectionString;
-		public DataManager(IConfiguration configuration)
-		{
-			string username = Environment.UserName;
+    public interface IDataManager
+    {
+        Task<int> Execute<T>(string sql, T item);
+        Task<bool> ExecuteWithTransaction(List<SqlCommand> dataRequest);
+        Task<IEnumerable<T>> Query<T, P>(string sql, P parameters);
+        Task<IEnumerable<T>> Query<T>(string sql);
+    }
 
-			_connectionString = configuration.GetConnectionString(username);
-		}
-		public async Task<int> Execute<T>(string sql, T item)
-		{
-			using IDbConnection connection = new SqlConnection(_connectionString);
-			return await connection.ExecuteAsync(sql, item);
-		}
-		public async Task<IEnumerable<T>> Query<T, P>(string sql, P parameters)
-		{
-			using IDbConnection connection = new SqlConnection(_connectionString);
-			return await connection.QueryAsync<T>(sql, parameters);
-		}
+    public class DataManager : IDataManager
+    {
+        private readonly string _connectionString;
+        public DataManager(IConfiguration configuration)
+        {
+            string username = Environment.UserName;
+
+            _connectionString = configuration.GetConnectionString(username);
+        }
+        public async Task<int> Execute<T>(string sql, T item)
+        {
+            using IDbConnection connection = new SqlConnection(_connectionString);
+            return await connection.ExecuteAsync(sql, item);
+        }
+        public async Task<IEnumerable<T>> Query<T, P>(string sql, P parameters)
+        {
+            using IDbConnection connection = new SqlConnection(_connectionString);
+            return await connection.QueryAsync<T>(sql, parameters);
+        }
 
         public async Task<IEnumerable<T>> Query<T>(string sql)
         {

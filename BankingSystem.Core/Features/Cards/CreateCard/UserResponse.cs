afa9c21bda81be6aa5b9182040fd179008029caa
zangura <zanguraa@gmail.com>
2024-03-26 16:50:48 +0400

    update Cards folder, add CreateCard folder with in service and repository file

---
+++ BankingSystem.Core/Features/Cards/CreateCard/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BankingSystem.Core.Features.Cards.CreateCard
+{
+    public class UserResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- BankingSystem.Core/Features/Cards/UserResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankingSystem.Core.Features.Cards
-{
-    public class UserResponse
-	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-	}
-}

33cea11062a218ea3f32b7e4ac76abb4636c8fa2
zangura <zanguraa@gmail.com>
2024-03-28 18:36:23 +0400

    add Test in UserService

---
+++ BankingSystem.Test/Features/Users/CreateUser/CreateUserTest.cs
@@ -0,0 +1,41 @@
+﻿
+using BankingSystem.Core.Data.Entities;
+using FakeItEasy;
+using Microsoft.AspNetCore.Identity;
+
+namespace BankingSystem.Test.Features.Users.CreateUser
+{
+    public class CreateUserTest
+    {
+        //_userManager.FindByEmailAsync(registerRequest.Email);
+
+        //_userManager.CreateAsync(newUser, registerRequest.Password);
+
+        //public void ShouldReturnUserIfitalreadyExists()
+        //{
+        //    var fakeUser = new UserEntity()
+        //    {
+        //        Email = "myEmail"
+        //    };
+
+        //    List<UserEntity> userList = new();
+
+        //    A.CallTo(() => _userManager.CreateAsync(Ignored)).
+        //        Invokes(() => userList.Add(fakeUser));
+
+        //    _userManager.CreateAsync(fakeUser);
+
+        //    A.CallTo(()=> _userManager.FindByEmailAsync(A<string>.Ignored))
+        //        .ReturnsLazy((string email) =>
+        //        {
+        //            var found = userList.FirstOrDefault(u=>u.Email==email);
+        //            return found;
+        //        });
+
+        //    var result = userservice.create();
+
+        //    Assert.That(fakeUser, result);
+        //}
+
+    }
+}

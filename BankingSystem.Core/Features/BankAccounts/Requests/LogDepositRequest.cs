a11878c03e61faae19fe5b870bc21c0c7e5fb27c
zangura <zanguraa@gmail.com>
2024-03-29 01:53:38 +0400

    delete LogDepositRequest from BankAccounts folder

--- BankingSystem.Core/Features/BankAccounts/Requests/LogDepositRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace BankingSystem.Core.Features.BankAccounts.Requests;
-public class LogDepositRequest
-{
-    public int BankAccountId { get; set; }
-    public decimal Amount { get; set; }
-    public DateTime Date { get; set; } = DateTime.Now;
-}

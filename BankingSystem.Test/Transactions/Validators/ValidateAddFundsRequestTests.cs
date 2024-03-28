a915b4a4f8df9ca4f322565cc0513b09c20eee81
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-28 04:01:56 +0400

    Add ValidateAddFundsRequestTests.cs file full implementation

--- BankingSystem.Test/Transactions/Validators/ValidateAddFundsRequestTests.cs
+++ BankingSystem.Test/Transactions/Validators/ValidateAddFundsRequestTests.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using BankingSystem.Core.Features.BankAccounts.AddFunds;
+using BankingSystem.Test.Factory;
+using System;
+using BankingSystem.Core.Shared.Exceptions;
+using FakeItEasy;
+using BankingSystem.Core.Features.Transactions.TransactionsRepositories;
 
-namespace BankingSystem.Test.Transactions.Validators
+namespace BankingSystem.Test.Features.BankAccounts.AddFunds
 {
-	internal class ValidateAddFundsRequestTests
+	[TestFixture]
+	public class ValidateAddFundsRequestTests
 	{
+		private IAddFundsService _addFundsService;
+		private IAddFundsRepository _fakeAddFundsRepository;
+		private ITransactionRepository _fakeTransactionRepository;
+
+		[SetUp]
+		public void Setup()
+		{
+			_fakeAddFundsRepository = A.Fake<IAddFundsRepository>();
+			_fakeTransactionRepository = A.Fake<ITransactionRepository>();
+			_addFundsService = new AddFundsService(_fakeAddFundsRepository, _fakeTransactionRepository);
+		}
+
+		[Test]
+		public void When_AddFundsRequestIsNull_ShouldThrow_ArgumentNullException()
+		{
+			AddFundsRequest request = null;
+
+			var ex = Assert.ThrowsAsync<ArgumentNullException>(() => _addFundsService.AddFunds(request));
+			Assert.That(ex.ParamName, Is.EqualTo("request"));
+			Assert.That(ex.Message, Does.Contain("The request cannot be null."));
+		}
+
+		[Test]
+		public void When_AmountIsLessThanOrEqualToZero_ShouldThrow_InvalidAddFundsValidationException()
+		{
+			var request = ModelFactory.GetAddFundsRequest(r => r.Amount = 0);
+
+			Assert.ThrowsAsync<InvalidAddFundsValidationException>(() => _addFundsService.AddFunds(request));
+		}
 	}
 }

a1e8099cb60775beac15329b3cf97bafb578f687
IRAKLII1231 <irakli.svanidze.1530@gmail.com>
2024-03-29 23:20:35 +0400

    Change structure in Test project and also Update ValidateChangePinAsyncTests.cs

---
+++ BankingSystem.Test/Features/Atm/CardAuthorization/ValidateCardAuthorizationTests.cs
@@ -0,0 +1,63 @@
+﻿using BankingSystem.Core.Features.Atm.CardAuthorizations;
+using BankingSystem.Core.Features.Atm.CardAuthorizations.Models.Requests;
+using BankingSystem.Core.Shared.Exceptions;
+using BankingSystem.Test.Factory;
+using NUnit.Framework;
+using FakeItEasy;
+using System.Threading.Tasks;
+using BankingSystem.Core.Shared;
+
+namespace BankingSystem.Test.Features.Atm.CardAuthorizations;
+
+[TestFixture]
+public class ValidateCardAuthorizationTests
+{
+	private ICardAuthorizationService _service;
+	private ICardAuthorizationRepository _repository;
+	private IJwtTokenGenerator _jwtTokenGenerator;
+	private ISeqLogger _logger;
+
+	[SetUp]
+	public void SetUp()
+	{
+		_repository = A.Fake<ICardAuthorizationRepository>();
+		_jwtTokenGenerator = A.Fake<IJwtTokenGenerator>();
+		_logger = A.Fake<ISeqLogger>();
+		_service = new CardAuthorizationService(_repository, _jwtTokenGenerator, _logger);
+	}
+
+	[Test]
+	public async Task AuthorizeCardAsync_WithNullRequest_ThrowsArgumentNullException()
+	{
+		Assert.ThrowsAsync<ArgumentNullException>(async () => await _service.AuthorizeCardAsync(null));
+	}
+
+	[TestCase("")]
+	[TestCase(null)]
+	public async Task AuthorizeCardAsync_WithInvalidCardNumber_ThrowsInvalidCardException(string cardNumber)
+	{
+		var request = ModelFactory.GetCardAuthorizationRequest(r => r.CardNumber = cardNumber);
+		Assert.ThrowsAsync<InvalidCardException>(async () => await _service.AuthorizeCardAsync(request));
+	}
+
+	[Test]
+	public async Task AuthorizeCardAsync_WithInvalidPin_ThrowsInvalidCardException()
+	{
+		var request = ModelFactory.GetCardAuthorizationRequest(r => r.Pin = -1);
+		Assert.ThrowsAsync<InvalidCardException>(async () => await _service.AuthorizeCardAsync(request));
+	}
+
+	[Test]
+	public async Task AuthorizeCardAsync_WithInvalidCardNumberLength_ThrowsInvalidCardException()
+	{
+		var request = ModelFactory.GetCardAuthorizationRequest(r => r.CardNumber = "12345");
+		Assert.ThrowsAsync<InvalidCardException>(async () => await _service.AuthorizeCardAsync(request));
+	}
+
+	[Test]
+	public async Task AuthorizeCardAsync_WithNonNumericCardNumber_ThrowsInvalidCardException()
+	{
+		var request = ModelFactory.GetCardAuthorizationRequest(r => r.CardNumber = new string('a', 16));
+		Assert.ThrowsAsync<InvalidCardException>(async () => await _service.AuthorizeCardAsync(request));
+	}
+}
